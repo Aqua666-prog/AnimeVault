ea90176464b5b8587f3e7e8be7464f0b478066ea
Aqua666-prog <alisakurakova397@gmail.com>
2026-08-17 19:37:00 +0300

    Fix Gradle repositories for GitHub Actions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,26 +1,16 @@
 pluginManagement {
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id.startsWith("com.android.") && requested.version != null) {
-                useModule("com.android.tools.build:gradle:${requested.version}")
-            }
-        }
-    }
     repositories {
-        maven {
-            url = uri("http://127.0.0.1:18080/unified")
-            isAllowInsecureProtocol = true
-        }
+        google()
+        mavenCentral()
+        gradlePluginPortal()
     }
 }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven {
-            url = uri("http://127.0.0.1:18080/unified")
-            isAllowInsecureProtocol = true
-        }
+        google()
+        mavenCentral()
     }
 }
 
